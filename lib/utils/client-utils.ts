5881463bfd8f01cce263a4a589d326d15598b9b9
Azekowka <gabjtov@gmail.com>
2025-07-14 01:48:13 +0500

    dead code clean up

--- lib/utils/client-utils.ts
+++
@@ -1,7 +0,0 @@
-
-export function createPathname(userId: string | undefined, locationId: string): string {
-  if (!userId) {
-    throw new Error('CreatePathname requires a userId')
-  }
-  return `${userId}/${locationId}/`
-}
